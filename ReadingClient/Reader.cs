b27eae4e28befb4490571009ff3801b9c1eb618c
Aaron <a.rcottrill521@gmail.com>
2018-05-07 13:20:11 -0400

    Port an old program to C#.

---
+++ ReadingClient/Reader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+/*
+ * A class designed to read files from a file
+ */
+namespace ReadingClient
+{
+    public class Reader
+    {
+        private String FilePath { get; set; } 
+
+
+        /*
+         * Instaniates a new Reader object and sets a default FilePath
+         */
+        public Reader()
+        {
+            FilePath = @"C:\Users\aaron\source\repos\ReadingClient\ReadingClient\users.txt";
+        }
+
+        /*
+        * Instaniates a new Reader object and sets the path to NewPath
+        */
+        public Reader(String NewPath)
+        {
+            FilePath = NewPath;
+        }
+
+        /*
+        * Reads a file from the specified file path set in the constructor. 
+        */
+        public String ReadFile()
+        {
+            //File file = new File("users.txt");
+            StringBuilder completeString = new StringBuilder();
+            //Scanner newScanner;
+
+            try
+            {
+                string[] lines = File.ReadAllLines(FilePath);
+
+                foreach (string line in lines)
+                {
+                    completeString.Append(line + "\n");
+                }
+
+                // newScanner = new Scanner(file);
+                // while (newScanner.hasNextLine()) // test for the end of the file
+                // {
+                //     completeString.append(newScanner.nextLine() + "\n");
+                // }
+                // newScanner.close();
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.Error.WriteLine(e.Message);
+                //e.Message;
+                //Console.Error.WriteLine("users.txt not found");
+            }
+
+            return completeString.ToString().Replace("=", ": ");
+        }
+    }
+}
---
+++ ReadingClient/ReaderDriver.cs
@@ -0,0 +1,57 @@
+﻿/**
+ *This program reads input from a file that uses 
+ *Ampersands to seperate data. Then that 
+ *data gets printed out. 
+ */
+
+using System;
+using System.IO;
+using System.Text;
+
+namespace ReadingClient
+{
+    public class ReaderDriver
+    {
+        
+        static void Main(string[] args)
+        {
+            Reader reader; 
+            if (args == null || args.Length == 0)
+            {
+                reader = new Reader(); 
+            }
+            else
+            {
+                reader = new Reader(args[0]);
+            }
+            
+
+            String unprocessedString = reader.ReadFile();
+
+            String[] lines = unprocessedString.Split("\n");
+
+            String[][] fields = new String[lines.Length - 1][];
+
+
+
+            for (int line = 0; line < lines.GetLength(0) - 1; line++)
+            {
+                fields[line] = lines[line].Split("&");
+            }
+
+            for (int row = 0; row < fields.Length; ++row)
+            {
+                for (int column = 0; column < fields[row].Length ; ++column)
+                {
+                    Console.Write(fields[row][column] + "\n");
+                }
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+
+        }
+
+       
+    }
+}
